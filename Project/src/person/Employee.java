eb92005aa8aaa355eef76115fb1fd6d7421188ec
lindi-imre <lindi.imre@gmail.com>
2016-03-16 12:30:31 +0100

    Written employee, person, skill class

--- Project/src/person/Employee.java
+++ Project/src/person/Employee.java
@@ -2,5 +2,27 @@ package person;
 
 public class Employee extends Person
 {
+	private int salary;
+	private String jobTitle;
+
+	public int getSalary()
+	{
+		return salary;
+	}
+
+	public void setSalary(int salary)
+	{
+		this.salary = salary;
+	}
+
+	public String getJobTitle()
+	{
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle)
+	{
+		this.jobTitle = jobTitle;
+	}
 
 }
--- Project/src/person/Skill.java
+++ Project/src/person/Skill.java
@@ -2,5 +2,29 @@ package person;
 
 public class Skill
 {
+	private String name;
+	private String description;
+	private double rate;
+
+	public Skill(String name, String description)
+	{
+		this.name = name;
+		this.description = description;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public double getRate()
+	{
+		return rate;
+	}
 
 }
